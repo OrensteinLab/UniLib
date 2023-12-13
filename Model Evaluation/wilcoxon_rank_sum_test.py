d5bf39b936503f041b9791910c1d785b2e348e69
Jonathan-Mandl <118123854+Jonathan-Mandl@users.noreply.github.com>
2023-12-13 18:02:45 +0200

    Update wilcoxon_rank_sum_test.py

--- Model Evaluation/wilcoxon_rank_sum_test.py
+++ Model Evaluation/wilcoxon_rank_sum_test.py
@@ -1,11 +1,8 @@
 import pandas as pd
 import scipy.stats as stats
-import numpy as np
 from statsmodels.stats import multitest
 
-
 def main():
-
     all_data = pd.read_csv("unilib_variant_bindingsites_KM_mean_0_sorted.csv")
 
     group1 = pd.read_csv("unilib_variant_bindingsites_KM_mean_0_pop1.csv")
@@ -21,14 +18,14 @@ def main():
     motifs = list(motifs_table['Motif sequence'])  # read motifs from file
     motifs.remove('GAATATTCTAGAATATTC')  # remove the motif with 0 matches in the data
 
-    # dictionary which will contain all variants containing each motif
+    # dictionary which will contain mean fl values for all variants containing each motif
     motif_variants = {}
 
     # find all variants containing each motif
     for motif in motifs:
         # use mask to find variant indexes containing the motifs
         contains_motif_mask = all_data['101bp sequence'].str.contains(motif)
-        contain_motif = all_data[contains_motif_mask]['Mean Fl'] # group of variants which contain specific motif
+        contain_motif = all_data[contains_motif_mask]['Mean Fl']  # group of variants which contain specific motif
         motif_variants[motif] = contain_motif
 
     motifs.append('desert motif')
@@ -40,25 +37,26 @@ def main():
     motif_variants['desert motif'] = contains_desert
 
     # sort motifs by median of mean FL
-    motifs = sorted(motifs, key=lambda x: motif_variants[x].median(axis=0), reverse=True)
+    motifs = sorted(motifs, key=lambda x: motif_variants[x].median(axis=0),
+                    reverse=True)  # sort motifs by median mean fl in reverse order
 
-    p_values=[]
+    p_values = []
 
     for i in range(len(motifs)):
         motif = motifs[i]
-        median=motif_variants[motif].median()
-        print(motif,median)
-        contain_motif=motif_variants[motif]
+        median = motif_variants[motif].median()
+        print(motif, median)
+        contain_motif = motif_variants[motif] # all variants containing motif
         other_motifs = all_data
         # remove all motifs in -5 or plus 5 in the ranking by median from the original motif
-        for close_motif in motifs[max(0, i-5):min(i+6, len(motifs))]:
-             other_motifs=other_motifs[~other_motifs['101bp sequence'].str.contains(close_motif)]
-        statistic, p_value = stats.mannwhitneyu(contain_motif, other_motifs['Mean Fl'])
+        for close_motif in motifs[max(0, i - 5):min(i + 6, len(motifs))]:
+            other_motifs = other_motifs[~other_motifs['101bp sequence'].str.contains(close_motif)]
+        statistic, p_value = stats.mannwhitneyu(contain_motif, other_motifs['Mean Fl']) # perform Wilcoxon rank sum test
         p_values.append(p_value)
 
-    p_value_df=pd.DataFrame()
+    p_value_df = pd.DataFrame()
 
-    p_value_df["P_values"]=p_values
+    p_value_df["P_values"] = p_values
 
     p_values_df = pd.DataFrame(p_values, index=motifs)
 
@@ -68,10 +66,11 @@ def main():
     # Add the rejection column to the DataFrame
     p_value_df['Rejected Benjamini Hochberg'] = rejected
 
-    p_value_df['Corrected p value']=corrected_p_values
+    p_value_df['Corrected p value'] = corrected_p_values
 
     # Save the DataFrame to a CSV file
     p_value_df.to_csv('p_values_no_close_motifs.csv', index_label='Motif')
+
 
 if __name__ == '__main__':
     main()
