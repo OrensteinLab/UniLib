013826a911d877436eff436b597dd4e51c7f556b
Jonathan-Mandl <118123854+Jonathan-Mandl@users.noreply.github.com>
2023-12-16 12:00:48 +0200

    Update MBO_model.py

--- Model Evaluation/Model_Code/MBO_model.py
+++ Model Evaluation/Model_Code/MBO_model.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 import random
 import os
 
-seed_value=42
+seed_value=21
 
 os.environ['PYTHONHASHSEED']=str(seed_value)
 os.environ['TF_DETERMINISTIC_OPS'] = '1'
@@ -47,23 +47,30 @@ def oneHotDeg(string):
     return one_hot_matrix
 
 
-def train_predict(train_sequences, train_labels, train_weights, test_data1, test_data2):
-    """
-    Run an ensemble of machine learning models and make predictions on test sequences.
+def train_predict_300_test():
 
-    Parameters:
-    - train_sequences: Training DNA sequences.
-    - train_labels: Labels for the training data.
-    - train_weights: Weights for the training data.
-    - test_sequences1: Test DNA sequences (300 variants).
-    - comp_test_sequences1: Reverse complement of test sequences 1.
-    - test_sequences2: Test DNA sequences (11 variants).
-    - comp_test_sequences2: Reverse complement of test sequences 2.
+    # read 300 validation variants from csv file
+    test = pd.read_csv("300_test_variants.csv")
+    test_sequences = list(test['101bp sequence'])  # read sequences
+    test_sequences = np.array(list(map(oneHotDeg, test_sequences)))  # use one hot function
+    # read labels
+    test_labels =test['Mean_FL']
 
-    Returns:
-    - predictions300 (list): Predictions for the 300 variants.
-    - predictions11 (list): Predictions for the 11 variants.
-    """
+    # read 2098 variants data with mixed bases and 22 barcodes
+    train = pd.read_csv("MBO_dataset.csv")
+    ##  remove 300 test sequences from all sequences
+    train = train[~train['101bp sequence'].isin(list(test['101bp sequence']))]
+    train_sequences = list(train['101bp sequence'])  # read sequences
+    train_sequences = np.array(list(map(oneHotDeg, train_sequences)))  # turn sequences to one hot vectors
+
+    # read labels & normalize
+    mean_fl_train = train['Mean_FL']
+    labels_train = np.array(mean_fl_train / max(mean_fl_train))
+
+    # define sample weights
+    weights = np.array(train['total_reads'])
+    weights = np.log(weights)
+    weights = weights / max(weights)
 
     # Initialize a new convolutional network model
     cnn_model = Sequential()
@@ -73,6 +80,52 @@ def train_predict(train_sequences, train_labels, train_weights, test_data1, test
     cnn_model.add(Dense(1, activation='linear'))
     cnn_model.compile(optimizer='adam', loss='mse')
 
+    # Shuffle the data
+    shuffled_indices = np.random.permutation(range(len(train_sequences)))
+
+    # Use the shuffled indices to access data while keeping their relative order
+    sequences_shuffled = train_sequences[shuffled_indices]
+    labels_shuffled = labels_train[shuffled_indices]
+    weights_shuffled = weights[shuffled_indices]
+
+    # Fit the model on shuffled data
+    cnn_model.fit(sequences_shuffled, labels_shuffled, epochs=3, batch_size=batch_size, verbose=1,
+                  sample_weight=weights_shuffled, shuffle=True)
+
+    predictions = cnn_model.predict(test_sequences)
+    predictions = [pred[0] for pred in predictions]
+
+    correlation,p_value= pearsonr(predictions,test_labels)
+
+    print("Correlation on 300 test: ",correlation, " p value: ",p_value)
+
+    test["MBO_model_prediction"] = predictions
+
+    test.to_csv('MBO_predictions_test_300.csv', index=False)
+
+
+
+def ensemble_model(train_sequences, train_labels, train_weights, test_sequences):
+    """
+    Run an ensemble of machine learning models and make predictions on test sequences.
+
+    Parameters:
+    - train_sequences: Training DNA sequences.
+    - train_labels: Labels for the training data.
+    - train_weights: Weights for the training data.
+    - test_sequences: Test DNA sequences (300 variants).
+
+    Returns:
+    - predictions (list): Predictions for the 11 variants.
+    """
+
+    # Initialize a new convolutional network model
+    cnn_model = Sequential()
+    cnn_model.add(Conv1D(filters=1024, kernel_size=6, strides=1, activation='relu', input_shape=(101, 4), use_bias=True))
+    cnn_model.add(GlobalMaxPooling1D())
+    cnn_model.add(Dense(16, activation='relu'))
+    cnn_model.add(Dense(1, activation='linear'))
+    cnn_model.compile(optimizer='adam', loss='mse')
 
     # Shuffle the data
     shuffled_indices = np.random.permutation(range(len(train_sequences)))
@@ -86,37 +139,28 @@ def train_predict(train_sequences, train_labels, train_weights, test_data1, test
     cnn_model.fit(sequences_shuffled, labels_shuffled, epochs=3, batch_size=batch_size, verbose=1,
                          sample_weight=weights_shuffled, shuffle=True)
 
-    predictions1 = cnn_model.predict(test_data1)
-    predictions1=[pred[0] for pred in predictions1]
-    predictions2 = cnn_model.predict(test_data2)
-    predictions2 = [pred[0] for pred in predictions2]
+    predictions = cnn_model.predict(test_sequences)
+    predictions=[pred[0] for pred in predictions]
 
-    return predictions1, predictions2
-
+    return predictions
 
 
 def main():
 
-    # read 300 validation variants
-    test1 = pd.read_csv("300_test_variants.csv")
-    test_sequences1 = list(test1['101bp sequence'])  # read sequences
-    test_sequences1 = np.array(list(map(oneHotDeg, test_sequences1)))  # use one hot function
-    # read labels
-    test_labels1 =test1['Mean_FL']
-
+    ## train model generate predictions for 300 test variants
+    train_predict_300_test()
 
     # read 11 validation variants
-    test2 = pd.read_csv('11_validation_variants.csv')
-    test_sequences2 = list(test2['sequence'])  # read sequences
+    validation = pd.read_csv('11_validation_variants.csv')
+    validation_sequences = list(validation['sequence'])  # read sequences
     # exclude 15-nt barcode from the variant sequence
-    test_sequences2 = [sequence[15:] for sequence in test_sequences2]
-    test_sequences2 = np.array(list(map(oneHotDeg, test_sequences2)))  # turn to one hot vectors
+    validation_sequences= [sequence[15:] for sequence in validation_sequences]
+    validation_sequences = np.array(list(map(oneHotDeg, validation_sequences)))  # turn to one hot vectors
     # read labels
-    test_labels2 = test2['mean_fl']
+    validation_labels = validation['mean_fl']
 
     # read 2098 variants data with mixed bases and 22 barcodes
     train = pd.read_csv("MBO_dataset.csv")
-    train=train[~train['101bp sequence'].isin(list(test1['101bp sequence']))] # remove test sequences from train dataset
     train_sequences = list(train['101bp sequence'])  # read sequences
     train_sequences = np.array(list(map(oneHotDeg, train_sequences)))  # turn sequences to one hot vectors
 
@@ -129,38 +173,27 @@ def main():
     weights = np.log(weights)
     weights = weights / max(weights)
 
-
-    all_predictions1 = []
-    all_predictions2 = []
+    all_predictions_val = []
 
     # run 100 models with random initialization as part of the random ensemble initialization technique
     for i in range(100):
         # Use the function to train model on MBO dataset with random initializations and make predictions
-        predictions1, predictions2 = train_predict(train_sequences, labels_train, weights,test_sequences1,test_sequences2)
-        all_predictions1.append(predictions1)
-        all_predictions2.append(predictions2)
+        predictions = ensemble_model(train_sequences, labels_train, weights,validation_sequences)
+        all_predictions_val.append(predictions)
 
     # calculate mean over the predictions of the 100 ensemble models
-    avg_predictions1 = np.mean(all_predictions1, axis=0)
-    avg_predictions2 = np.mean(all_predictions2, axis=0)
+    avg_predictions_val = np.mean(all_predictions_val, axis=0)
 
     # calculate pearson correlation on validation sets
-    corr1,p_value1  = pearsonr(avg_predictions1, test_labels1)
-    corr2,p_value2 = pearsonr(avg_predictions2, test_labels2)
+    corr,p_value  = pearsonr(avg_predictions_val, validation_labels)
 
     # print pearson correlation & p-value
-    print("Correlations on 11: ", corr2, "P value: ",p_value2)
-    print("Correlations on 300: ", corr1, "P value: ",p_value1)
+    print("Correlations on 11: ", corr, "P value: ",p_value)
 
     # create columns for the 100 models average predictions and the true labels
-    test1["Average_model_prediction"] = avg_predictions1
-    test2["Average_model_prediction"] = avg_predictions2
-    test1["True_labels"] = test_labels1
-    test2["True_labels"] = test_labels2
+    validation["Average_MBO_prediction"] = avg_predictions_val
 
-    # Save the DataFrame with true labels verses predictions to a CSV file
-    test1.to_csv('test_300_predictions_MBO.csv', index=False)
-    test2.to_csv('test_11_predictions_MBO.csv', index=False)
+    validation.to_csv('MBO_predictions_11_validation.csv', index=False)
 
 if __name__ == "__main__":
     main()
